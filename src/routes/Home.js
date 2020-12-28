3786dd75386a254f7cb74ea76022d8a056453955
hyo814 <ggamjige8888@naver.com>
2020-12-28 11:20:21 +0900

    사진 미리 보기 업로드 준비 중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
 import Hweet from "components/Hweet";
 
 const Home = ({ userObj }) => {
     const [hweet, setHweet] = useState("");
     const [hweets, setHweets] = useState([]);
-
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
         dbService.collection("hweets").onSnapshot((snapshot) => {
             const hweetArray = snapshot.docs.map((doc) => ({
@@ -17,6 +18,9 @@ const Home = ({ userObj }) => {
     }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
+        const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+        const response = await fileRef.putString(attachment, "data_url");
+        console.log(response);
         await dbService.collection("hweets").add({
             hweet,
             text: hweet,
@@ -31,6 +35,23 @@ const Home = ({ userObj }) => {
         } = event;
         setHweet(value);
     };
+    //파일 미리 보기
+    const onFileChange = (event) => {
+        const {
+            target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () =>
+        setAttachment(null);
 
     return (
         <div className="container">
@@ -41,12 +62,19 @@ const Home = ({ userObj }) => {
                     type="text"
                     placeholder="무슨 일이 일어날까요?"
                     maxLength={120}
-                />&nbsp;
+                />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Hweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div style={{ marginTop: 50 }}>
                 {hweets.map((hweet) => (
-                     <Hweet
+                    <Hweet
                         key={hweet.id}
                         hweetObj={hweet}
                         isOwner={hweet.creatorId === userObj.uid}
